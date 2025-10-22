036e1031369358fcfb850abbb03ab05dee0071f8
RCA04 <ryan04.dev@gmail.com>
2025-10-22 10:04:53 -0300

    develop: configurações das migrations

--- back-end/backend/database/migrations/2025_10_22_123515_create_tasks_table.php
+++ back-end/backend/database/migrations/2025_10_22_123515_create_tasks_table.php
@@ -11,12 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('project_id');
+            $table->string('title');
             $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
             $table->date('due_date')->nullable();
             $table->timestamps();
+
+
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
         });
     }
 
@@ -25,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('tasks');
     }
 };
