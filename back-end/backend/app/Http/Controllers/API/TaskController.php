0d8bd14d2b944b73d0fce482cc3ec5e35f4ea500
RCA04 <ryan04.dev@gmail.com>
2025-10-22 13:51:02 -0300

    develop: configs dos metodos da resouce api

--- back-end/backend/app/Http/Controllers/API/TaskController.php
+++ back-end/backend/app/Http/Controllers/API/TaskController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -12,7 +14,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $task = Task::with('project')->get();
+        return response()->json($task, 200);
     }
 
     /**
@@ -20,7 +23,21 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'project_id' => 'required|exists:projects,id',
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'in:pending,in_progress,completed',
+            'due_date' => 'nullable|date',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $task = Task::create($request->all());
+
+        return response()->json($task, 201);
     }
 
     /**
@@ -28,7 +45,14 @@ class TaskController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $task = Task::with('project')->find($id);
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
+
+        return response()->json($task, 200);
+        
+
     }
 
     /**
@@ -36,7 +60,31 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $task = Task::find($id);
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'project_id' => 'sometimes|required|exists:projects,id',
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'in:pending,in_progress,completed',
+            'due_date' => 'nullable|date',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $task->projeto_id = $request->project_id;
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        return response()->json($task, 200);
     }
 
     /**
