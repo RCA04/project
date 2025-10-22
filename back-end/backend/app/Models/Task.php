d893f2a555f3cb25aaca999edb22e333c801f86e
RCA04 <ryan04.dev@gmail.com>
2025-10-22 10:04:27 -0300

    develop: adição dos campos fillables

--- back-end/backend/app/Models/Task.php
+++ back-end/backend/app/Models/Task.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+    
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
