26f5381edc56c74bed06af037f9beb63814e6ee3
RCA04 <ryan04.dev@gmail.com>
2025-10-23 11:04:26 -0300

    develop: adição das rotas de navegeção

--- front-end/frontend/src/App.jsx
+++ front-end/frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import { Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contacts from "./pages/contacts";
+import { Link } from "react-router-dom";
+
 function App(){
   return(
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      
+      <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contacts'>Contacts</Link>
+      </nav>
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    
+    </Router>
   )
 }
 
