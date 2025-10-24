89167df596f023fad3917d5b3e4a12d07551ecc5
RCA04 <ryan04.dev@gmail.com>
2025-10-24 10:07:49 -0300

    develop: filtragem das rotas de adição da rota dashboard

--- front-end/frontend/src/App.jsx
+++ front-end/frontend/src/App.jsx
@@ -1,25 +1,39 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contacts from "./pages/contacts";
 import Navbar from "./components/navbar";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
 
-function App(){
+function Layout(){
+  const location = useLocation();
+
+  //show Navbar only on specific routes
+  const hideNavbarRoutes = ["/login", "/register" , "/"].includes(location.pathname);
+
+
   return(
-    <BrowserRouter>
-      <Navbar/>
-    
-    
+    <>
+    {!hideNavbarRoutes && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* <Route path="/about" element={<About />} />
         <Route path="/contacts" element={<Contacts />} /> */}
       </Routes>
-    
+    </>
+  );
+
+}
+
+function App(){
+  return(
+    <BrowserRouter>
+      <Layout />
     </BrowserRouter>
   )
 }
