e01f00d00d61b0adaf8b13475148cf318fcf5fed
RCA04 <ryan04.dev@gmail.com>
2025-10-24 11:15:18 -0300

    develop: correção do import do componente

--- front-end/frontend/src/components/protectedRoute/index.jsx
+++ front-end/frontend/src/components/protectedRoute/index.jsx
@@ -1,12 +1,12 @@
 import { Children } from "react";
 import { Navigate } from "react-router-dom";
 
-export default ProtectedRoute({Children}){
+export default function ProtectedRoute({children}) {
     const token = localStorage.getItem("token");
 
     if(!token){
         return <Navigate to="/login" replace />;
-    }
+    };
 
-    return Children;
+    return children;
 }
