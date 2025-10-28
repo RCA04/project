c0035fe285deaab3d94e86ee1b5b54ffe5040063
RCA04 <ryan04.dev@gmail.com>
2025-10-28 06:09:29 -0300

    develop: pesquisa também somente pelos item do usuário logado

--- back-end/backend/app/Http/Controllers/API/DashboardController.php
+++ back-end/backend/app/Http/Controllers/API/DashboardController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function stats(){
+        return response()->json([
+            'projects_count' => Project::where('user_id', auth()->id())->count(),
+            'tasks_count' => Task::where('user_id', auth()->id())->count()    
+        ]);
+        }
 }
