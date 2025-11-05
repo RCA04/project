1a6cdd362dedd08e0e77e4ca19520457739991ae
RCA04 <ryan04.dev@gmail.com>
2025-11-05 10:31:57 -0300

    develop: criação do campo de foto do usuario

--- back-end/backend/app/Http/Controllers/API/AuthController.php
+++ back-end/backend/app/Http/Controllers/API/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
@@ -18,16 +19,25 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-        ]);
+            'photo'=>'nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:2048'
+ ]);
+
+        $photoPath = $request->hasfile('photo')
+        ? $request->file('photo')->store('photos', 'public')
+        : null;
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
         $user = User::create($request->all());
+        $user = User::create([
+            'profile_photo'=>$photoPath
+        ]);
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json(['message' => 'User registered successfully',
             'user' => $user,
+            'profile_photo'=>$photoPath ? Storage::url($photoPath): null,
             'token' => $token,
         ], 201);
     }
@@ -64,4 +74,6 @@ class AuthController extends Controller
 
         return response()->json(['message' => 'User logged out successfully'], 200);
     }
+
+
 }
